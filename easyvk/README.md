054244ee1b2487799e81a5a97d674860c0ee9e69
mandryskowski <nedesf@gmail.com>
2026-01-05 01:49:23 +0100

    Turn easyvk into submodule

--- easyvk/README.md
+++
@@ -1,2 +0,0 @@
-# easyvk
-Layer to simplify use of Vulkan for lightweight GPU compute applications.
--- easyvk/example/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_BUILD_SCRIPT := Android.mk
-APP_STL := c++_static # TODO: Not sure what this should be.
-APP_ABI := all 
-APP_CPPFLAGS := -fexceptions
-APP_PLATFORM := 24
